bf8c8de02dd40284d8895e1f51586027ef7f2e9d
marshki <marshall@morethantomorrow.org>
2017-12-18 11:49:06 -0500

    update test

--- tests/test.py
+++ tests/test.py
@@ -6,15 +6,14 @@ random = Random()
 def num_gen():
     return random.randint(1, 100)
 
-
 class num_gen_Test(unittest.TestCase):
 
     def setUp(self):
         global random
         random = Random(666)
 
-    def test_num_age(self):
-        self.assertEqual(num_gen(), 46)
+    def test_num_gen(self):
+        self.assertEqual(num_gen(), 59)
 
 if __name__ == '__main__':
     unittest.main()
