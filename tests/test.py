d1fed9b2b3042b917644bb08cb9c399181e98ba6
marshki <marshall@morethantomorrow.org>
2017-12-19 23:50:46 -0500

    update test.py

--- tests/test.py
+++ tests/test.py
@@ -6,6 +6,14 @@ random = Random()
 def num_gen():
     return random.randint(1, 100)
 
+
+def user_guess():
+    while True:
+        try:
+    return int(input('Enter a guess:  '))
+        except ValueError:
+    print('Sorry, try again.')
+
 class num_gen_Test(unittest.TestCase):
 
     def setUp(self):
@@ -14,6 +22,8 @@ class num_gen_Test(unittest.TestCase):
 
     def test_num_gen(self):
         self.assertEqual(num_gen(), 59)
+
+    def test_user_guess(self): 
 
 if __name__ == '__main__':
     unittest.main()
