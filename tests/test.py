b6e310cd339c004622df9f6848a01059b6f40e33
marshki <marshall@morethantomorrow.org>
2017-12-24 09:00:56 -0500

    combine unitests

--- tests/test.py
+++ tests/test.py
@@ -1,18 +1,21 @@
-from random import Random
+#!/usr/bin/env python
+
 import unittest
+
+from random import Random
+from unittest.mock import patch
+from unittest import TestCase
 
 random = Random()
 
 def num_gen():
     return random.randint(1, 100)
 
-
-def user_guess():
+def play_again():
     while True:
-        try:
-    return int(input('Enter a guess:  '))
-        except ValueError:
-    print('Sorry, try again.')
+            again = input('Play again? Y/N:  ').lower()
+            if again == 'y' or again == 'n':
+                    return again
 
 class num_gen_Test(unittest.TestCase):
 
@@ -23,7 +26,18 @@ class num_gen_Test(unittest.TestCase):
     def test_num_gen(self):
         self.assertEqual(num_gen(), 59)
 
-    def test_user_guess(self): 
+class play_again_Test(unittest.TestCase):
+
+    def get_input(text):
+        return input(text)
+
+    @patch('builtins.input', return_value='y')
+    def test_play_again_yes(self, input):
+        self.assertEqual(play_again(), 'y')
+
+    @patch('builtins.input', return_value='n')
+    def test_play_again_no(self, input):
+        self.assertEqual(play_again(), 'n')
 
 if __name__ == '__main__':
     unittest.main()
