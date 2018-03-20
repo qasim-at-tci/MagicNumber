c087a9180ddf59fbaea3e4de1f8343e955f61c7f
marshki <marshall@morethantomorrow.org>
2018-03-20 09:38:36 -0400

    add play_again unit test

--- tests/play_again.py
+++ tests/play_again.py
@@ -1,5 +1,8 @@
 #!/user/bin/python 
+import unittest
 
+from unittest.mock import patch 
+from unittest import TestCase
 
 def play_again():
     while True:
@@ -7,4 +10,18 @@ def play_again():
         if again == 'y' or again == 'n':
             return again
 
-play_again()
+class play_again_Test(unittest.TestCase):
+
+    def get_input(self):
+        return input(self)
+
+    @patch('builtins.input', return_value='y')
+    def test_play_again_yes(self, input):
+        self.assertEqual(play_again(), 'y')
+
+    @patch('builtins.input', return_value='n')
+    def test_play_again_no(self, input):
+        self.assertEqual(play_again(), 'n')
+
+if __name__ == '__main__':
+    unittest.main()
