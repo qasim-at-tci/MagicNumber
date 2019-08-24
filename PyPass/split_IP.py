c9e2040acfb0bad22b88459a7f8e06ae5775873b
marshki <marshall@morethantomorrow.org>
2019-08-24 19:10:14 -0400

    update

--- PyPass/split_IP.py
+++ PyPass/split_IP.py
@@ -4,9 +4,12 @@
 import ipaddress
 
 addr = '159.65.178.169'
+print(addr)
 
 octet = addr.split('.',4)
-
 print(octet[2], octet[3], octet[1])
 
+octet_int = list(map(int, octet))
+
+print(octet_int[2], octet_int[3], octet_int[1])
 
