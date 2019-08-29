86175dae9bcfdc4619de55258a0bc55d1503281f
marshki <marshall@morethantomorrow.org>
2019-08-29 01:24:30 -0400

    update

--- PyPass/split_IP.py
+++ PyPass/split_IP.py
@@ -7,7 +7,7 @@ address = '159.65.178.169'
 print(address)
 
 strip_address = address.split('.',4)
-print(strip_address[2])
+print(strip_address)
 
 octet_8 = ((strip_address[2] + '*' + str(int(strip_address[3]) + 8)).ljust(8, '*')) 
 print(octet_8)
