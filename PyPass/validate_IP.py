3f538279c3e2ce06799fe260f6a548e63cd0c1f8
marshki <marshall@morethantomorrow.org>
2019-08-31 19:25:50 -0400

    clean up via PEP 8

--- PyPass/validate_IP.py
+++ PyPass/validate_IP.py
@@ -3,6 +3,7 @@
 
 import ipaddress
 
+
 def validate_IP():
     """Prompt user for IPv4 address, then validate."""
 
@@ -12,18 +13,21 @@ def validate_IP():
         except ValueError:
             print('Bad value, try again.')
 
-def short_password(): 
+
+def eight_bit():
     """ Transform IP address to 8-bit password."""
-    return((split_address[2] + '*' + str(int(split_address[3]) + 8)).ljust(8, '*')) 
+    return((split_address[2] + '*' 
+    + str(int(split_address[3]) + 8)).ljust(8, '*'))
 
-def long_password(): 
+
+def twelve_bit():
     """ Transform IP address to 12-bit password."""
-    return((split_address[2] + '*' + str(int(split_address[3]) + 12) + '*' + split_address[1]).ljust(12, '*')) 
+    return((split_address[2] + '*' 
+    + str(int(split_address[3]) + 12) + '*' + split_address[1]).ljust(12, '*'))
 
-if __name__ == '__main__': 
+
+if __name__ == '__main__':
     address = validate_IP()
-    split_address = address.split('.',4)
-    print(short_password())
-    print(long_password())
-
-# print("{} is a valid IPv4 address, continuing...".format(address))
+    split_address = address.split('.', 4)
+    print(' 8-bit:    12-bit: ')
+    print(eight_bit(), twelve_bit())
