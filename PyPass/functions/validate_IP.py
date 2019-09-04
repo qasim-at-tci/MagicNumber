eff2b805a6cb53ce42e92e3d6bcc10946d1c976d
marshki <marshall@morethantomorrow.org>
2019-09-03 20:44:49 -0400

    update all

---
+++ PyPass/functions/validate_IP.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3 
+# https://docs.python.org/3/library/ipaddress.html
+
+import ipaddress 
+import unittest
+
+from unittest.mock import patch
+from unittest import TestCase
+
+
+def validate_IP(): 
+    """Prompt user for IPv4 address, then validate."""
+
+    while True: 
+        try: 
+            return str(ipaddress.IPv4Address(input('Enter a valid IPv4 address: ')))
+        except ValueError: 
+            print('Bad value, try again.') 
+
+
+class validate_IP_Test(unittest.TestCase): 
+
+    def get_input(self): 
+        return input(self)
+
+    @patch('builtins.input', return_value='192.168.1.1')
+    def test_validate_IP(self, input):
+        self.assertEqual(validate_IP(), '192.168.1.1')
+
+    @patch('builtins.input', return_value='10.0.0.1')
+    def test_validate_IP(self, input):
+        self.assertEqual(validate_IP(), '10.0.0.1')
+
+
+if __name__ == '__main__':
+    unittest.main()
+
---
+++ PyPass/window.py
@@ -0,0 +1,84 @@
+#!/bin/py
+#Python2 OR 3
+
+try:
+    import tkinter as tk
+except ImportError:
+    import Tkinter as tk
+
+def 8_bit(8_bit_in): 
+    """ 8-bit password generator. """
+
+def 12_bit(12_bit_in): 
+    """ 12-bit password generator. """
+
+def convert_f2c(f_in):
+    """ Convert the value in temp_data from Fahrenheit to Celsius and store the result in out_data. """
+    return ((f_in - 32) * 5 / 9)
+
+def convert_c2f(c_in):
+    """ Convert the value in temp_data from Celsius to Fahrenheit and store the result in out_data. """
+    return ((c_in * 1.8) + 32)
+
+class MainWindow(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        master.config(menu=MenuBar(self))
+
+        self.mode = 'f2c'
+
+        self.label_in = tk.Label(self, text='Temperature in Fahrenheit:')
+        self.label_in.pack()
+
+        self.temp_in = tk.DoubleVar(0)
+        text = tk.Entry(self, textvar=self.temp_in)
+        text.pack()
+
+        self.temp_out = tk.StringVar(self, '-17')
+        lbl = tk.Label(self, textvar=self.temp_out)
+        lbl.pack()
+
+        btn = tk.Button(self, text='Convert', command=self.convert)
+        btn.pack()
+
+        btn = tk.Button(self, text='Quit', command=master.destroy)
+        btn.pack()
+
+    def f2c_mode(self):
+        self.mode = 'f2c'
+        self.label_in.config(text='Temperature in Fahrenheit:')
+
+    def c2f_mode(self):
+        self.mode = 'c2f'
+        self.label_in.config(text='Temperature in Celsius:')
+
+    def convert(self):
+        try:
+            if self.mode == 'f2c':
+                temp_out = convert_f2c(self.temp_in.get())
+            else:
+                temp_out = convert_c2f(self.temp_in.get())
+            self.temp_out.set("{:.3f}".format(temp_out))
+        except ValueError as e:
+            self.temp_out.set("ERROR: {}".format(e))
+
+class MenuBar(tk.Menu):
+    def __init__(self, master):
+        tk.Menu.__init__(self, master)
+
+        file_menu = tk.Menu(self, tearoff=False)
+        self.add_cascade(label='File', menu=file_menu)
+        file_menu.add_command(label='F to C mode', command=master.f2c_mode)
+        file_menu.add_command(label='C to F mode', command=master.c2f_mode)
+        file_menu.add_separator()
+        file_menu.add_command(label='Exit', command=self.quit)
+
+def main():
+    window = tk.Tk()
+    window.title("Temp Utility")
+    frame = MainWindow(window)
+    frame.pack()
+    window.mainloop()
+
+if __name__ == '__main__':
+    main()
