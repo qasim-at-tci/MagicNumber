9b0052fa4792877012cea180429a71af67329add
marshki <marshall@morethantomorrow.org>
2019-09-18 12:20:24 -0400

    fix pylint stuff

--- PyPass/functions/validate_IP.py
+++ PyPass/functions/validate_IP.py
@@ -1,24 +1,24 @@
-#!/usr/bin/env python3 
+#!/usr/bin/env python3
 # https://docs.python.org/3/library/ipaddress.html
 
-import ipaddress 
+import ipaddress
 import unittest
 
 from unittest.mock import patch
 from unittest import TestCase
 
-def validate_IP(): 
+def validate_IP():
     """Prompt user for IPv4 address, then validate."""
 
-    while True: 
-        try: 
+    while True:
+        try:
             return ipaddress.IPv4Address(input('Enter a valid IPv4 address: '))
-        except ValueError: 
-            print('Bad value, try again.') 
-            raise 
+        except ValueError:
+            print('Bad value, try again.')
+            raise
 
 
-class validate_IP_Test(unittest.TestCase): 
+class validate_IP_Test(unittest.TestCase):
 
     @patch('builtins.input', return_value='192.168.1.1')
     def test_validate_IP_01(self, input):
@@ -31,8 +31,8 @@ class validate_IP_Test(unittest.TestCase):
     @patch('builtins.input', return_value='Derp!')
     def test_validate_IP_03(self, input):
         with self.assertRaises(ValueError): 
-            validate_IP() 
-             
+            validate_IP()
+ 
 
 if __name__ == '__main__':
     unittest.main()
