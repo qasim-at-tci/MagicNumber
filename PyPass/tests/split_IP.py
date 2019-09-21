5e6d8486350919ddae53fe297bc5c2b13a14ad8c
marshki <marshall@morethantomorrow.org>
2019-09-21 13:06:44 -0400

    update

--- PyPass/tests/split_IP.py
+++ PyPass/tests/split_IP.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
-address = '159.65.178.169'
-print(address)
+ADDRESS = '159.65.178.169'
+print(ADDRESS)
 
-strip_address = address.split('.',4)
-print(strip_address)
+STRIP_ADDRESS = ADDRESS.split('.', 4)
+print(STRIP_ADDRESS)
 
-octet_8 = ((strip_address[2] + '*' + str(int(strip_address[3]) + 8)).ljust(8, '*')) 
-print(octet_8)
+OCTET_8 = ((STRIP_ADDRESS[2] + '*' + str(int(STRIP_ADDRESS[3]) + 8)).ljust(8, '*'))
+print(OCTET_8)
 
-octet_12 = ((strip_address[2] + '*' + str(int(strip_address[3]) + 12) + '*' + strip_address[1]).ljust(12, '*')) 
-print(octet_12)
+OCTET_12 = ((STRIP_ADDRESS[2] + '*' + str(int(STRIP_ADDRESS[3]) + 12) + '*' + STRIP_ADDRESS[1]).ljust(12, '*')) 
+print(OCTET_12)
