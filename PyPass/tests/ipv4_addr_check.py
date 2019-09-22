ad69ee1b334bb08543be1f0cd9eadbc352452d50
marshki <marshall@morethantomorrow.org>
2019-09-22 07:36:05 -0400

    comments

--- PyPass/tests/ipv4_addr_check.py
+++ PyPass/tests/ipv4_addr_check.py
@@ -11,16 +11,7 @@ import unittest
 from unittest.mock import patch
 
 def ipv4_addr_check():
-    """Prompt user for IPv4 address, then validate.
-
-    Args:
-        arg1: input
-
-    Raises:
-        ValueError: `Bad Value, try again.`
-
-    Returns:
-        class 'ipaddress.IPv4Address' if valid, re-prompt if not.
+    """Prompt user for IPv4 address, then validate. Re-prompt if bad value.
     """
 
     while True:
@@ -34,49 +25,24 @@ def ipv4_addr_check():
 class IPv4AddrCheckTest(unittest.TestCase):
 
     """Unit tests.
-
     Use `patch()` to mock objects for testing.
     """
 
     @patch('builtins.input', return_value='192.168.1.1')
     def test_ipv4_addr_check_01(self, input):
         """Valid return value.
-
-        Agrs:
-            arg1: self
-            arg2: input
-
-        Returns:
-            class 'ipaddress.IPv4Address'
-
         """
         self.assertIsInstance(ipv4_addr_check(), ipaddress.IPv4Address)
 
     @patch('builtins.input', return_value='10.0.0.1')
     def test_ipv4_addr_check_02(self, input):
         """Valid return value.
-
-        Args:
-            arg1: self
-            arg2: input
-
-        Returns:
-            class 'ipaddress.IPv4Address'
-
         """
         self.assertIsInstance(ipv4_addr_check(), ipaddress.IPv4Address)
 
     @patch('builtins.input', return_value='Derp!')
     def test_ipv4_addr_check_03(self, input):
         """Invalid return value.
-
-        Args:
-            arg1: self
-            arg2: input
-
-        Returns:
-            Bad value, try again.
-
         """
         with self.assertRaises(ValueError):
             ipv4_addr_check()
