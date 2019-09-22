31cb2312913c64251441d066eb50f45b415f165b
marshki <marshall@morethantomorrow.org>
2019-09-22 09:11:58 -0400

    add expected failure tests

--- PyPass/tests/split_ip.py
+++ PyPass/tests/split_ip.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python3
 
-"""Docstring.
+"""Convert IP address to 8- or 12-bit password.
 """
 
 import unittest
 
-
-ADDRESS = '159.65.178.169'
-
+ADDRESS = '192.168.1.1'
 SPLIT_ADDRESS = ADDRESS.split('.', 4)
 
 
 def eight_bit_passwd():
-    """ Transform IP address to 8-bit password."""
+    """Transform IP address to 8-bit password.
+    """
     return((SPLIT_ADDRESS[2] + '*' +
             str(int(SPLIT_ADDRESS[3]) + 8)).ljust(8, '*'))
 
 
 def twelve_bit_passwd():
-    """ Transform IP address to 12-bit password."""
+    """Transform IP address to 12-bit password.
+    """
     return((SPLIT_ADDRESS[2] + '*' +
             str(int(SPLIT_ADDRESS[3]) + 12) + '*' + SPLIT_ADDRESS[1]).ljust(12, '*'))
 
@@ -27,5 +27,27 @@ class SplitIPTest(unittest.TestCase):
     """Unit tests.
     """
 
+    def test_eight_bit_passwd_01(self):
+        """Valid return value.
+        """
+        self.assertEqual(eight_bit_passwd(), '1*9*****')
 
- 
+    @unittest.expectedFailure
+    def test_eight_bit_passwd_02(self):
+        """Invalid return value.
+        """
+        self.assertEqual(eight_bit_passwd(), '1*13*168****')
+
+    def test_twelve_bit_passwd_01(self):
+        """Valid return value.
+        """
+        self.assertEqual(twelve_bit_passwd(), '1*13*168****')
+
+    @unittest.expectedFailure
+    def test_twelve_bit_passwd_02(self):
+        """Invalid return value.
+        """
+        self.assertEqual(twelve_bit_passwd(), '1*9*****')
+
+if __name__ == '__main__':
+    unittest.main()
