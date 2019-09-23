42e1691ced8bac79772382fc44c80abd999e8768
marshki <marshall@morethantomorrow.org>
2019-09-23 09:04:43 -0400

    update

--- PyPass/tests/passwd_gen.py
+++ PyPass/tests/passwd_gen.py
@@ -29,20 +29,17 @@ import unittest
 ADDRESS = '192.168.1.1'
 SPLIT_ADDRESS = ADDRESS.split('.', 4)
 
-
 def eight_bit_passwd():
     """Transform IP address to 8-bit password.
     """
     return((SPLIT_ADDRESS[2] + '*' +
             str(int(SPLIT_ADDRESS[3]) + 8)).ljust(8, '*'))
 
-
 def twelve_bit_passwd():
     """Transform IP address to 12-bit password.
     """
     return((SPLIT_ADDRESS[2] + '*' +
             str(int(SPLIT_ADDRESS[3]) + 12) + '*' + SPLIT_ADDRESS[1]).ljust(12, '*'))
-
 
 class SplitIPTest(unittest.TestCase):
     """Unit tests.
