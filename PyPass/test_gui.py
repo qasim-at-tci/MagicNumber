a52ef3c83db2474725492e801e6922e62b272226
marshki <marshall@morethantomorrow.org>
2019-10-26 12:32:38 -0400

    update

--- PyPass/test_gui.py
+++ PyPass/test_gui.py
@@ -15,4 +15,11 @@ EVENT, VALUES = WINDOW.Read()
 WINDOW.Close()
 
 TEXT_INPUT = VALUES[0]
-sg.Popup('IPass: ', TEXT_INPUT)
+
+SPLIT_ADDRESS = TEXT_INPUT.split('.', 4)
+
+EIGHT_BIT_PASS = SPLIT_ADDRESS[2] + '*' + str(int(SPLIT_ADDRESS[3]) + 8).ljust(8, '*')
+
+sg.Popup('8-bit: ', EIGHT_BIT_PASS)
+
+# SPLIT_ADDRESS = TEXT_INPUT.split('.', 4)
