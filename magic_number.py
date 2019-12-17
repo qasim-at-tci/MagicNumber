66a6114165cdfa2710f667fcb30710db536a0d5a
marshki <marshall@morethantomorrow.org>
2019-12-17 17:33:31 -0500

    fix pylint stuff

--- magic_number.py
+++ magic_number.py
@@ -10,66 +10,65 @@ import random
 MAX_ATTEMPTS = 5
 
 def generate_random_number():
-	"""
-	Return random number between 1 and 100, inclusive.
-	"""
-	return random.randint(1, 100)
+    """Return random number between 1 and 100, inclusive.
+    """
+    return random.randint(1, 100)
 
 def user_guess():
-	"""
-	Prompt player, limiting input to integer.
-	Return input.
-	"""
-	while True:
-		try:
-			return int(input('Enter a guess:  '))
-		except ValueError:
-			print('Input must be an integer. Try again.')
+    """Prompt player, limiting input to integer.
+    Return input.
+    """
+
+    while True:
+        try:
+            return int(input('Enter a guess:  '))
+        except ValueError:
+            print('Input must be an integer. Try again.')
 
 def evaluate_user_guess():
-	"""
-	Compare user_guess to magic_number.
-	Provide feedback.
-	"""
-	magic_number = generate_random_number()
+    """Compare user guess to "magic" number.
+    Provide feedback.
+    """
 
-	for attempt in range(MAX_ATTEMPTS):
-		guess = user_guess()
+    magic_number = generate_random_number()
 
-		if guess < magic_number:
-			print('Higher...')
-		elif guess > magic_number:
-			print('Lower...')
-		else:
-			print('That\'s right!')
-			break
+    for attempt in range(MAX_ATTEMPTS):
+        guess = user_guess()
 
-	if guess != magic_number:
-		print('Out of guesses! The magic number was: {}.'.format(magic_number))
+        if guess < magic_number:
+            print('Higher...')
+        elif guess > magic_number:
+            print('Lower...')
+        else:
+            print('That\'s right!')
+            break
+
+    if guess != magic_number:
+        print('Out of guesses! The magic number was: {}.'.format(magic_number))
 
 def play_again():
+    """Prompt player, limiting input to "Y" or "N".
+    Return input.
     """
-    Prompt player, limiting input to "Y" or "N".
-	Return input.
-    """
+
     while True:
-            again = input('Play again? Y/N:  ').lower()
-            if again == 'y' or again == 'n':
-                    return again
+        again = input('Play again? Y/N:  ').lower()
+        if again == 'y' or again == 'n':
+            return again
 
 def game_play():
-	"""
-	Play game. Allow user to play multiple rounds or resign.
-	"""
-	while True:
-		evaluate = evaluate_user_guess()
-		another_round = play_again()
+    """Play game. Allow user to play multiple rounds or resign.
+    """
+    while True:
+        evaluate = evaluate_user_guess()
+        another_round = play_again()
 
-		if another_round == 'y':
-			continue
-		else:
-			break
+        if another_round == 'y':
+            continue
+        else:
+            break
 
 if __name__ == '__main__':
-	print("Welcome to the \"Magic\" number game!\nTry to guess the magic number (1-100) in 5 attempts or less.")
-	game_play()
+    print("Welcome to the \"Magic\" number game!\
+          \nTry to guess the magic number (1-100) in 5 attempts or less.")
+    game_play()
