2b4895aa299185f3030fc1aa580a280b54775937
marshki <marshall@morethantomorrow.org>
2019-12-18 11:08:39 -0500

    address pylint issues

--- tests/user_guess.py
+++ tests/user_guess.py
@@ -1,21 +1,30 @@
 #!/usr/bin/env python
 
+"""Placeholder.
+"""
+
 import unittest
 
-from unittest.mock import patch 
+from unittest.mock import patch
 from unittest import TestCase
 
-def user_guess(): 
-    while True: 
-        try: 
+def user_guess():
+    """Placeholder.
+    """
+    while True:
+        try:
             return int(input('Enter a guess: '))
-        except ValueError: 
+        except ValueError:
             print('Sorry, try again.')
 
-class user_guess_Test(unittest.TestCase): 
+class UserGuessTest(unittest.TestCase):
+    """Placeholder.
+    """
 
     @patch('builtins.input', return_value='16')
-    def test_user_guess(self, input): 
+    def test_user_guess(self, input):
+        """Placeholder.
+        """
         self.assertTrue(user_guess(), int)
 
 if __name__ == '__main__':
