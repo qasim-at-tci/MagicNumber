abce504f9955ba9fda4180cae751e22ee45b9d00
marshki <marshall@morethantomorrow.org>
2019-12-18 10:57:54 -0500

    address pylint issues

--- tests/rando.py
+++ tests/rando.py
@@ -1,21 +1,29 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
+
+"""Placeholder.
+"""
 
 import unittest
 from random import Random
 
 def num_gen():
+    """Return random number between 1 and 100, inclusive.
     """
-    Return random number between 1 and 100, inclusive
-    """
-    return random.randint(1, 100) 
+    return random.randint(1, 100)
 
-class num_gen_Test(unittest.TestCase):
+class NumGenTest(unittest.TestCase):
+    """Place holder.
+    """
 
     def setUp(self):
+        """Placeholder.
+        """
         global random
         random = Random(123)
 
     def test_num_gen(self):
+        """Placeholder.
+        """
         self.assertEqual(num_gen(), 7)
 
 if __name__ == '__main__':
