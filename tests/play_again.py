08ffdddbc960d5ee19e303361476adb3d88a9895
marshki <marshall@morethantomorrow.org>
2019-12-18 10:46:54 -0500

    address pylint issues, still need to fix test

--- tests/play_again.py
+++ tests/play_again.py
@@ -1,26 +1,40 @@
-#!/user/bin/python 
+#!/user/bin/python
+
+"""Test to validate user input.
+"""
+
+from builtins import input
 import unittest
 
-from unittest.mock import patch 
+from unittest.mock import patch
 from unittest import TestCase
 
 def play_again():
+    """Prompt user for input.
+    Reprompt if input not y or n.
+    """
     while True:
-        again = input('Play again? Y/N:  ').lower()
-        if again == 'y' or again == 'n':
+        again = input('Play again? Y/N: ').lower()
+        if again in ('y', 'n'):
             return again
 
-class play_again_Test(unittest.TestCase):
+class PlayAgainTest(unittest.TestCase):
+    """Unit tests.
+    """
 
     def get_input(self):
         return input(self)
 
     @patch('builtins.input', return_value='y')
     def test_play_again_yes(self, input):
+        """Valid return value.
+        """
         self.assertEqual(play_again(), 'y')
 
     @patch('builtins.input', return_value='n')
     def test_play_again_no(self, input):
+        """Valid return value.
+        """
         self.assertEqual(play_again(), 'n')
 
 if __name__ == '__main__':
