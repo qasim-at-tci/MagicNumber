eb355c0fd88ea496405fcda07fd5cd522838e79a
marshki <marshall@morethantomorrow.org>
2019-12-22 08:36:31 -0500

    clean up unit test, still WIP

--- tests/play_again.py
+++ tests/play_again.py
@@ -28,7 +28,7 @@ class PlayAgainTest(unittest.TestCase):
         self.assertEqual(play_again(), 'y')
 
 
-    @patch('builtins.input', return_value='n')
+    @patch('builtins.input', return_value='N')
     def test_play_again_02(self, input):
         """Valid return value.
         """
