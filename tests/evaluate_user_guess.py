ed45304baf7d5709b0b47efb153a70f0dab9479c
marshki <marshall@morethantomorrow.org>
2020-01-10 13:47:55 -0500

    update

--- tests/evaluate_user_guess.py
+++ tests/evaluate_user_guess.py
@@ -2,29 +2,30 @@
 
 """Placeholder.
 """
-
+import random
 #import unittest
 #from unittest.mock import patch
 
-MAGIC_NUMBER = 2
+MAGIC_NUMBER = 83
 MAX_ATTEMPTS = 5
-GUESS = ('50', '25', '12', '6', '3')
-list(GUESS)
+GUESS = [1, 58, 71, 77, 86]
 
 def evaluate_user_guess():
     """Compare user guess to "magic" number.
     Provide feedback.
     """
-
     for _i in range(MAX_ATTEMPTS):
-        for _i in(GUESS):
-            if GUESS < MAGIC_NUMBER:
-                print('Higher...')
-            elif GUESS > MAGIC_NUMBER:
-                print('Lower...')
-            else:
-                print('That\'s right!')
-                break
+        current_guess = random.choice(GUESS)
+
+        if current_guess < MAGIC_NUMBER:
+            print(current_guess)
+            print('Higher...')
+        elif current_guess > MAGIC_NUMBER:
+            print(current_guess)
+            print('Lower...')
+        else:
+            print('That\'s right!')
+            break
 
     if GUESS != MAGIC_NUMBER:
         print('Out of guesses! The magic number was: {}.'.format(MAGIC_NUMBER))
