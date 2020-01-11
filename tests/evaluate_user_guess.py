dd290eb6b2914b1e6338ee86ecb70dbc3499a69f
marshki <marshall@morethantomorrow.org>
2020-01-11 10:45:23 -0500

    fix pylint errors

--- tests/evaluate_user_guess.py
+++ tests/evaluate_user_guess.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# pylint: disable=w0622
+# pylint: disable=r1710,w0613,w0622
 
 """Placeholder.
 """
@@ -8,9 +8,9 @@ import random
 import unittest
 from unittest.mock import patch
 
-MAGIC_NUMBER = 83
+MAGIC_NUMBER = 50
 MAX_ATTEMPTS = 5
-GUESS = [1, 58, 71, 77, 86]
+GUESS = [1, 38, 41, 77, 86]
 
 def evaluate_user_guess():
     """Compare user guess to "magic" number.
@@ -20,11 +20,11 @@ def evaluate_user_guess():
         current_guess = random.choice(GUESS)
 
         if current_guess < MAGIC_NUMBER:
-            return 'Higher...'
+            print('Higher...')
         elif current_guess > MAGIC_NUMBER:
-            return 'Lower...'
+            print('Lower...')
         else:
-            return 'That\'s right!'
+            print('That\'s right!')
 
     if GUESS != MAGIC_NUMBER:
         return 'Out of guesses!'
